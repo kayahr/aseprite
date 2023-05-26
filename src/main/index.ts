f1745472a4184aa3438690b297016a3f499838bc
Klaus Reimer <k@ailis.de>
2023-05-26 15:16:40 +0200

    Switch to named exports and type exports so loaders like stealjs don't load the empty interface javascript files

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,21 +3,20 @@
  * See LICENSE.md for licensing information.
  */
 
-import "./aseprite.json";
+import "./json";
 
-export * from "./ArrayFrame";
-export * from "./BlendMode";
-export * from "./Color";
-export * from "./Direction";
-export * from "./Format";
-export * from "./Frame";
-export * from "./FrameTag";
-export * from "./index";
-export * from "./Layer";
-export * from "./Meta";
-export * from "./Point";
-export * from "./Rectangle";
-export * from "./Size";
-export * from "./Slice";
-export * from "./SliceKey";
-export * from "./SpriteSheet";
+export type { ArrayFrame } from "./ArrayFrame";
+export { BlendMode } from "./BlendMode";
+export type { Color } from "./Color";
+export { Direction } from "./Direction";
+export { Format } from "./Format";
+export type { Frame } from "./Frame";
+export type { FrameTag } from "./FrameTag";
+export type { Layer } from "./Layer";
+export type { Meta } from "./Meta";
+export type { Point } from "./Point";
+export type { Rectangle } from "./Rectangle";
+export type { Size } from "./Size";
+export type { Slice } from "./Slice";
+export type { SliceKey } from "./SliceKey";
+export type { SpriteSheet }  from "./SpriteSheet";
