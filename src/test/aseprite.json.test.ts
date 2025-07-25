88a60cdfa25b4633a8ebeb325eba58fbaf7e2977
Klaus Reimer <k@ailis.de>
2025-07-25 18:40:10 +0200

    Add unit test for repeat property

--- src/test/aseprite.json.test.ts
+++ src/test/aseprite.json.test.ts
@@ -2,6 +2,8 @@ describe("aseprite.json", () => {
     it("provides typings for *.aseprite.json files", async () => {
         const sprite = (await import("../../src/test/sprites/kayahr.aseprite.json")).default;
         expect(sprite.frames.length).toBeGreaterThan(0);
+        expect(sprite.meta.frameTags?.[0]?.repeat).toBe(undefined);
+        expect(sprite.meta.frameTags?.[1]?.repeat).toBe("2");
         expect(sprite.meta.image).toBe("kayahr.png");
     });
 });
